d3c73d311222e74b5e4736e081aa3554ebc36ca8
AnnabelZhang1 <azhang22@stuy.edu>
2020-12-06 17:52:43 -0500

    fixed

--- IntegerSequence.java
+++ IntegerSequence.java
@@ -1,9 +1,6 @@
-public class IntegerSequence{
-
-  public interface IntegerSequence{
-    boolean hasNext();//does the sequence have more elements?
-    int next();       //return the current value in the sequence and advances to the next element.
-    int length();     //returns the total length of the sequence
-    void reset();     //start over from the start of the sequence
-  }
+public interface IntegerSequence{
+  boolean hasNext();//does the sequence have more elements?
+  int next();       //return the current value in the sequence and advances to the next element.
+  int length();     //returns the total length of the sequence
+  void reset();     //start over from the start of the sequence
 }
